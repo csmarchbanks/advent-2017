400f02a194a60b6f71d0e2427e9fc3ddd0b0cbdc
Chris Marchbanks <csmarchbanks@gmail.com>
2017-12-20 21:51:10 -0700

    Solution to part two of day 14

--- day14/defragment.go
+++ day14/defragment.go
@@ -2,31 +2,86 @@ package main
 
 import (
 	"fmt"
-	"math/bits"
-	"strconv"
+	"math/big"
 
 	"github.com/csmarchbanks/advent-2017/utils"
 )
 
-func OnesCount(hashPart string) int {
-	val, err := strconv.ParseUint(hashPart, 16, 64)
-	if err != nil {
-		panic(err)
+type Grid [128][128]bool
+
+func OnesCount(hash string) (sum int) {
+	val := big.NewInt(0)
+	_, success := val.SetString(hash, 16)
+	if !success {
+		panic("Failed to set string")
 	}
-	return bits.OnesCount64(val)
+	for i := 0; i < val.BitLen(); i++ {
+		sum += int(val.Bit(i))
+	}
+	return
 }
 
 func UsedSquares(key string) (sum int) {
 	for i := 0; i < 128; i++ {
 		fullKey := fmt.Sprintf("%s-%d", key, i)
 		hash := utils.Hash(fullKey)
-		part1 := hash[0:16]
-		part2 := hash[16:32]
-		sum += OnesCount(part1) + OnesCount(part2)
+		sum += OnesCount(hash)
 	}
 	return
 }
 
+func CreateGrid(key string) Grid {
+	grid := Grid{}
+	for i := 0; i < 128; i++ {
+		fullKey := fmt.Sprintf("%s-%d", key, i)
+		hash := utils.Hash(fullKey)
+		val := big.NewInt(0)
+		val.SetString(hash, 16)
+		for j := 0; j < 128; j++ {
+			grid[i][127-j] = val.Bit(j) == 1
+		}
+	}
+	return grid
+}
+
+func getKey(i, j int) int {
+	return (i << 16) + j
+}
+
+func MapRegion(grid Grid, cache map[int]int, i, j, regionValue int) {
+	if i >= 0 && i < 128 && j >= 0 && j < 128 && grid[i][j] {
+		key := getKey(i, j)
+		if _, found := cache[key]; found {
+			return
+		} else {
+			cache[key] = regionValue
+		}
+		MapRegion(grid, cache, i+1, j, regionValue)
+		MapRegion(grid, cache, i-1, j, regionValue)
+		MapRegion(grid, cache, i, j+1, regionValue)
+		MapRegion(grid, cache, i, j-1, regionValue)
+	}
+}
+
+func CountRegions(key string) int {
+	regions := []int{}
+	cache := make(map[int]int)
+	grid := CreateGrid(key)
+	for i, row := range grid {
+		for j, v := range row {
+			key := getKey(i, j)
+			if _, found := cache[key]; v && !found {
+				regionValue := len(regions) + 1
+				regions = append(regions, regionValue)
+				MapRegion(grid, cache, i, j, regionValue)
+			}
+		}
+	}
+	return len(regions)
+}
+
 func main() {
-	fmt.Println(UsedSquares("ffayrhll"))
+	key := "ffayrhll"
+	fmt.Println(UsedSquares(key))
+	fmt.Println(CountRegions(key))
 }
--- day14/defragment_test.go
+++ day14/defragment_test.go
@@ -17,3 +17,19 @@ func TestUsedSquares(t *testing.T) {
 		}
 	}
 }
+
+func TestCountRegions(t *testing.T) {
+	tcs := []struct {
+		in  string
+		out int
+	}{
+		{"flqrgnkx", 1242},
+		{"ffayrhll", 1134},
+	}
+	for _, tc := range tcs {
+		result := CountRegions(tc.in)
+		if result != tc.out {
+			t.Errorf("UsedSquares(%s) => %d, expected %d\n", tc.in, result, tc.out)
+		}
+	}
+}
